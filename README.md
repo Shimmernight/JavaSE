de44d8aa79aeab562930cdb3d6415bfff0f338cc
shimmernight <53256477+shimmernight@users.noreply.github.com>
2020-05-12 10:29:55 +0800

    线程状态

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-## Getting Started
+# JavaSE基础内容
+* 内容介绍
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
-## Folder Structure
-
-The workspace contains two folders by default, where:
-
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
-
-## Dependency Management
-
-The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
+    本人学习JavaSE的学习代码
---
+++ src/Thread/state/TestC.java
@@ -0,0 +1,30 @@
+package Thread.state;
+
+//TODO设置守护线程
+//虚拟机不管守护线程是否在运行
+public class TestC {
+    public static void main(String[] args) {
+        Thread God = new Thread(() -> {
+            while (true) {
+                System.out.println("上帝守护你");
+            }
+        });
+        God.setDaemon(true);// 设置守护线程
+        God.start();
+
+        You you = new You();
+        new Thread(you).start();//用户线程
+    }
+
+    // 你
+    static class You implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 365; i++) {
+                System.out.println("你每天开心的活着");
+            }
+            System.out.println("你死了");
+        }
+    }
+}
